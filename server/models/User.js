e4fa6131210729f327ffead995b906d1de819b2f
codexdhruv11 <dhruvkaushikjee9457@gmail.com>
2025-02-11 17:17:37 +0530

    Readme updated with setup steps

--- server/models/User.js
+++ server/models/User.js
@@ -10,3 +10,4 @@ module.exports = mongoose.model('User', userSchema);
 // Update on 2025-02-16T03:10:04
 // Update on 2025-02-21T23:34:50
 // Update on 2025-02-12T06:18:48
+// Update on 2025-02-11T17:17:37
