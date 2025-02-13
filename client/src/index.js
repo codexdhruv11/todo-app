732ba70b3c75196715150fb0f0bf447f25b750d3
codexdhruv11 <dhruvkaushikjee9457@gmail.com>
2025-02-13 17:48:32 +0530

    Refactor task controller logic

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
+// Update on 2025-02-13T17:48:32
