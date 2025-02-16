46f596eb06cebf08bc0d7bcf21455837df0b8e8b
codexdhruv11 <dhruvkaushikjee9457@gmail.com>
2025-02-16 14:21:05 +0530

    PrivateRoute logic updated

---
+++ server/routes/taskRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const {
+  createTask, getTasks, updateTask, deleteTask, reorderTasks
+} = require('../controllers/taskController');
+
+router.post('/', auth, createTask);
+router.get('/', auth, getTasks);
+router.put('/:id', auth, updateTask);
+router.delete('/:id', auth, deleteTask);
+router.post('/reorder', auth, reorderTasks);
+
+module.exports = router;
+// Update on 2025-02-16T14:21:05
