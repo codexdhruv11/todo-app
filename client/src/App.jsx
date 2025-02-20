2080083472dea6712106862bb19c9f43891afd2d
codexdhruv11 <dhruvkaushikjee9457@gmail.com>
2025-02-20 19:53:44 +0530

    Context logic cleanup in AuthContext

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { TaskProvider } from './context/TaskContext';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+
+function App() {
+  return (
+    <AuthProvider>
+      <TaskProvider>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            } />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Router>
+      </TaskProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
+// Update on 2025-02-20T19:53:44
