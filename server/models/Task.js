d00e0374e5107f06672d3761437371e7f4c8a92d
codexdhruv11 <dhruvkaushikjee9457@gmail.com>
2025-02-21 11:59:33 +0530

    PrivateRoute logic updated

--- server/models/Task.js
+++ server/models/Task.js
@@ -12,3 +12,4 @@ const taskSchema = new mongoose.Schema({
 module.exports = mongoose.model('Task', taskSchema);
 // Update on 2025-02-11T15:12:33
 // Update on 2025-02-20T05:09:31
+// Update on 2025-02-21T11:59:33
