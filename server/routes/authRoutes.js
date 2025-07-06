fc4c85dfff37f606ff08059505b01cec5309ff48
Dhruv Kaushik <dhruvkaushikjee9457@gmail.com>
2025-07-06 15:34:45 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,12 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
 
-// User registration route
 router.post('/register', register);
 
-// User login route
 router.post('/login', login);
 
 module.exports = router;
-// Update on 2025-02-14T10:13:38
-// Update on 2025-02-19T04:49:31
